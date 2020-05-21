101e0c2bc74d33e8c29ca6c360779827d10985b2
gaylong9 <2236657293@qq.com>
2020-05-21 19:59:59 +0800

    upgrade permission settings

--- app/src/main/java/com/example/hp/MainActivity.java
+++ app/src/main/java/com/example/hp/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.hp;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,38 +19,82 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SmsManager smsManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SmsManager sms = SmsManager.getDefault();
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE)
-                != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS)
-                        != PackageManager.PERMISSION_GRANTED) {
-            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-            dialog.setTitle("请注意：");
-            dialog.setMessage("本程序支持从1065930051处获取闪讯密码。\n\n" +
-                    "使用前，请先将本程序的以下权限修改为“允许”：\n" +
-                    "○发送短信\n○获取手机信息\n\n" +
-                    "设置完成后再次启动时，自动退出为正常，可前往短信查看是否发送成功。");
-            dialog.setCancelable(false);
-            dialog.setPositiveButton("前往设置", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Intent intent =  new Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
-                    startActivity(intent);
-                    MainActivity.this.finish();
-                }
-            });
-            dialog.show();
+
+        smsManager = SmsManager.getDefault();
+
+        if (judgePermission()) {
+            // 有权限，直接发送
+            sendMessage();
         }else {
-            sms.sendTextMessage("1065930051",
-                    null, "MM", null, null);
-            Toast toast;
-            toast=Toast.makeText(MainActivity.this,null,Toast.LENGTH_SHORT);
-            toast.setText("短信发送成功");
-            toast.show();
-            MainActivity.this.finish();
+            // 没有权限，获取权限
+            createDialog();
         }
+    }
+
+    // 创建dialog，提醒并获取权限
+    private void createDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("请注意：");
+        dialog.setMessage(R.string.dialog_message);
+        dialog.setCancelable(false);
+        dialog.setNegativeButton("设置", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Intent intent =  new Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
+                Intent intent1 =  new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent1.setData(Uri.fromParts("package", getPackageName(), null));
+                startActivity(intent1);
+                MainActivity.this.finish();
+            }
+        });
+        dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[] {
+                            Manifest.permission.READ_PHONE_STATE,
+                            Manifest.permission.SEND_SMS
+                    }, 1);
+                }
+            }
+        });
+        dialog.show();
+    }
+
+    boolean judgePermission() {
+        return (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE)
+                == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS)
+                == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                sendMessage();
+            }
+            else {
+                MainActivity.this.finish();
+            }
+        }
+    }
+
+    private void sendMessage() {
+        smsManager.sendTextMessage("1065930051",
+                null, "MM", null, null);
+        Toast toast;
+        toast=Toast.makeText(MainActivity.this,null,Toast.LENGTH_SHORT);
+        toast.setText("短信发送成功");
+        toast.show();
+        MainActivity.this.finish();
     }
 }
